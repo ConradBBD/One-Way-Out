97c8ae2db24fe8bb93bcdd1799aea2410cf57298
ConradBBD <conrad@bbd.co.za>
2022-10-05 23:26:48 +0200

    Player animation and some background sound

--- One Way Out/Assets/Scripts/PlayerController.cs
+++ One Way Out/Assets/Scripts/PlayerController.cs
@@ -9,8 +9,9 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public Transform movePoint;
 
-    public LayerMask whatStopsMovement;
+    public LayerMask walls;
     public LayerMask fallOffCliff;
+    public LayerMask reachedExit;
 
     public Animator anim;
 
@@ -28,39 +29,40 @@ public class PlayerController : MonoBehaviour
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, walls))
                 {
                     if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, fallOffCliff))
                     {
                         movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                    } else
+                    } 
+                    else
                     {
                         SceneManager.LoadScene("GameOver");
                     }
                 }
-            } else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            } 
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, walls))
                 {
                     if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, fallOffCliff))
                     {
                         movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                    } else
+                    } 
+                    else
                     {
                         SceneManager.LoadScene("GameOver");
                     }
-                         
                 }
             }
-
-            //if (Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, fallOffCliff))
-            //{
-            //    SceneManager.LoadScene("GameOver");
-            //}
-            //if (Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, fallOffCliff))
-            //{
-            //    SceneManager.LoadScene("GameOver");
-            //}
+            anim.SetBool("moving", false);
+        } else
+        {
+            anim.SetBool("moving", true);
+        }
+        if (Physics2D.OverlapCircle(movePoint.position, .2f, reachedExit))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
